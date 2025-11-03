94378612fa3e8640c0931976391828353822176c
barakes <33425897+barakes@users.noreply.github.com>
2025-11-03 23:06:58 +0200

    Update task2.js

--- task2.js
+++ task2.js
@@ -3,18 +3,17 @@
 const num = 237;
 let flag;
 
-for (let i = 1; i <= num; i++) {
+for (let i = 2; i <= num; i++) {
   flag = true;
 
+  // Check divisibility from 2 up to i-1
   for (let j = 2; j < i && flag; j++) {
     if (i % j === 0) {
-      flag = false;
+      flag = false; // Found a divisor → not prime
     }
   }
 
   if (flag) {
-    console.log(i);
+    console.log(i); // Print the number if it's prime
   }
 }
-
-
